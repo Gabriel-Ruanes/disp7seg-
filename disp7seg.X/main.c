e6730fa8be010d47d1d38f5437631d6af7b2f26a
21192694 <21192694@DSN1271158611.cfp127aluno.local>
2021-02-24 14:08:39 -0300

    Programação para display de 7 seguimentos

---
+++ disp7seg.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: Gabriel Ruanes Mwlquiades 
+ *
+ * Created on 24 de Fevereiro de 2021, 14:04
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include "delay.h"
+
+void main(void) 
+{
+    while ( 1 )
+    {
+    
+    }
+}
